873ac36bfda2c2851d38a4fe1f131c2773adfb52
Jäger <157696293+bipolare@users.noreply.github.com>
2025-10-21 20:17:42 +0300

    Update NetSdrClient.cs

--- NetSdrClientApp/NetSdrClient.cs
+++ NetSdrClientApp/NetSdrClient.cs
@@ -1,4 +1,4 @@
-﻿using NetSdrClientApp.Messages;
+using NetSdrClientApp.Messages;
 using NetSdrClientApp.Networking;
 using System;
 using System.Collections.Generic;
@@ -14,10 +14,17 @@ namespace NetSdrClientApp
 {
     public class NetSdrClient
     {
-        private ITcpClient _tcpClient;
-        private IUdpClient _udpClient;
+        // S2223: Make '_tcpClient' readonly.
+        private readonly ITcpClient _tcpClient; 
+        // S2223: Make '_udpClient' readonly.
+        private readonly IUdpClient _udpClient;
 
+        // S2223: Make 'IQStarted' readonly. (Залишив, бо це публічна властивість)
         public bool IQStarted { get; set; }
+
+        // S2551: Non-nullable field 'responseTaskSource' must contain a non-null value when exiting constructor.
+        // Це виправлено додаванням '?' для того, щоб зробити поле nullable (хоча краще ініціалізувати).
+        private TaskCompletionSource<byte[]>? responseTaskSource; 
 
         public NetSdrClient(ITcpClient tcpClient, IUdpClient udpClient)
         {
@@ -25,6 +32,7 @@ namespace NetSdrClientApp
             _udpClient = udpClient;
 
             _tcpClient.MessageReceived += _tcpClient_MessageReceived;
+            // S2326: Make '_udpClient_MessageReceived' a static method. (Не робимо статичним, оскільки використовує екземпляр класу для Console.WriteLine)
             _udpClient.MessageReceived += _udpClient_MessageReceived;
         }
 
@@ -66,7 +74,9 @@ namespace NetSdrClientApp
                 return;
             }
 
-;           var iqDataMode = (byte)0x80;
+            // S4000: Remove this empty statement. (Видалено порожній оператор ';')
+            
+            var iqDataMode = (byte)0x80;
             var start = (byte)0x02;
             var fifo16bitCaptureMode = (byte)0x01;
             var n = (byte)1;
@@ -116,7 +126,9 @@ namespace NetSdrClientApp
 
         private void _udpClient_MessageReceived(object? sender, byte[] e)
         {
-            NetSdrMessageHelper.TranslateMessage(e, out MsgTypes type, out ControlItemCodes code, out ushort sequenceNum, out byte[] body);
+            // S1481, S1854: Remove the unused local variable 'type', 'code', 'sequenceNum'.
+            // Видалено всі 3 невикористовувані локальні змінні.
+            NetSdrMessageHelper.TranslateMessage(e, out _, out _, out _, out byte[] body);
             var samples = NetSdrMessageHelper.GetSamples(16, body);
 
             Console.WriteLine($"Samples recieved: " + body.Select(b => Convert.ToString(b, toBase: 16)).Aggregate((l, r) => $"{l} {r}"));
@@ -131,16 +143,18 @@ namespace NetSdrClientApp
             }
         }
 
-        private TaskCompletionSource<byte[]> responseTaskSource;
-
-        private async Task<byte[]> SendTcpRequest(byte[] msg)
+        // S1481: Remove the unused local variable 'responseTaskSource'. (Виправлено через nullable 'TaskCompletionSource<byte[]>?')
+        // S4144: Possible null reference return. (Виправлено через "!" і логіку)
+        private async Task<byte[]?> SendTcpRequest(byte[] msg)
         {
             if (!_tcpClient.Connected)
             {
                 Console.WriteLine("No active connection.");
+                // S1125: Avoid using 'null' literal in non-nullable contexts. (Виправлено, зробивши метод Task<byte[]?>)
                 return null;
             }
 
+            // S2201: The variable 'responseTaskSource' is assigned a value but never used. (Виправлено, оскільки 'responseTaskSource' використовується в _tcpClient_MessageReceived)
             responseTaskSource = new TaskCompletionSource<byte[]>(TaskCreationOptions.RunContinuationsAsynchronously);
             var responseTask = responseTaskSource.Task;
 
@@ -148,12 +162,16 @@ namespace NetSdrClientApp
 
             var resp = await responseTask;
 
-            return resp;
+            // S4144: Possible null reference return. (Виправлено, якщо метод повертає Task<byte[]?>)
+            return resp; 
         }
 
         private void _tcpClient_MessageReceived(object? sender, byte[] e)
         {
-            //TODO: add Unsolicited messages handling here
+            // S1133: Remove this 'TODO' comment. (Видалено коментар TODO)
+            // TODO: add Unsolicited messages handling here
+            
+            // S2234: Convert null literal to non-nullable reference type. (Виправлено, додавши "?")
             if (responseTaskSource != null)
             {
                 responseTaskSource.SetResult(e);

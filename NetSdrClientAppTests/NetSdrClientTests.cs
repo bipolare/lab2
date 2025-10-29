91c14df137abe476e752f4fb9c2539abc9fcfbb6
Jäger <157696293+bipolare@users.noreply.github.com>
2025-10-29 11:48:33 +0200

    Update NetSdrClientTests.cs

--- NetSdrClientAppTests/NetSdrClientTests.cs
+++ NetSdrClientAppTests/NetSdrClientTests.cs
@@ -51,7 +51,7 @@ public class NetSdrClientTests
     public async Task DisconnectWithNoConnectionTest()
     {
         //act
-        _client.Disconect();
+        _client.Disconnect();
 
         //assert
         //No exception thrown

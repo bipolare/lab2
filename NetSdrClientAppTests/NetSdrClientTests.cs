a5f2a8c6f0777b096e613dff924925bc4df371eb
Jäger <157696293+bipolare@users.noreply.github.com>
2025-10-29 11:52:19 +0200

    Fix typo in Disconnect method call

--- NetSdrClientAppTests/NetSdrClientTests.cs
+++ NetSdrClientAppTests/NetSdrClientTests.cs
@@ -65,7 +65,7 @@ public class NetSdrClientTests
         await ConnectAsyncTest();
 
         //act
-        _client.Disconect();
+        _client.Disconnect();
 
         //assert
         //No exception thrown
